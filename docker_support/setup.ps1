21820d45b057c0d03a91f8e220b3d5e81015fa85
James Fera <james.fera@gmail.com>
2017-01-02 22:21:47 -0500

    docker support files

--- docker_support/setup.ps1
+++ docker_support/setup.ps1
@@ -6,7 +6,7 @@ write-host
 
 .\post_run_commands.ps1
 
-.\docker_support\Get-IPAddresses.ps1
+.\docker_support\Get-Services.ps1 | Format-Table Name, Url
 
 write-host
 write-host "You will need to restart Explorer and Visual Studio in order for Environment Variable changes to take effect." -foregroundcolor "yellow"
