4f4e489a4118556bfe21a3d6b00f6303d7f86113
James Fera <james.fera@gmail.com>
2017-01-02 21:05:37 -0500

    docker support files

--- install_docker_support.ps1
+++ install_docker_support.ps1
@@ -6,7 +6,7 @@ iwr https://raw.githubusercontent.com/canerectors/buildsupport/master/docker_sup
 md .\docker_support >$null
 
 iwr https://raw.githubusercontent.com/canerectors/buildsupport/master/docker_support/docker_commands.ps1 -UseBasicParsing -OutFile .\docker_support\docker_commands.ps1
-iwr https://raw.githubusercontent.com/canerectors/buildsupport/master/docker_support/status.ps1 -UseBasicParsing -OutFile .\docker_support\status.ps1
+iwr https://raw.githubusercontent.com/canerectors/buildsupport/master/docker_support/Get-IPAddresses.ps1 -UseBasicParsing -OutFile .\docker_support\Get-IPAddresses.ps1
 iwr https://raw.githubusercontent.com/canerectors/buildsupport/master/docker_support/setup.ps1 -UseBasicParsing -OutFile .\docker_support\setup.ps1
 iwr https://raw.githubusercontent.com/canerectors/buildsupport/master/docker_support/launch.ps1 -UseBasicParsing -OutFile .\docker_support\launch.ps1
 iwr https://raw.githubusercontent.com/canerectors/buildsupport/master/docker_support/Get-Service-Urls.ps1 -UseBasicParsing -OutFile .\docker_support\Get-Service-Urls.ps1
