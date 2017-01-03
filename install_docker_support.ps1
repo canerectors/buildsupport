1b202af521df36d7ecc77f412df5e8b487d85780
James Fera <james.fera@gmail.com>
2017-01-02 21:22:38 -0500

    docker support files

--- install_docker_support.ps1
+++ install_docker_support.ps1
@@ -3,7 +3,7 @@ Write-Host "Installing Docker Support files to: $(Get-Location)\docker_support..
 #install dependencies
 iwr https://raw.githubusercontent.com/canerectors/buildsupport/master/docker_support/install_dependencies.ps1 -UseBasicParsing | iex
 
-md .\docker_support >$null
+md .\docker_support *> $null
 
 iwr https://raw.githubusercontent.com/canerectors/buildsupport/master/docker_support/docker_commands.ps1 -UseBasicParsing -OutFile .\docker_support\docker_commands.ps1
 iwr https://raw.githubusercontent.com/canerectors/buildsupport/master/docker_support/Get-IPAddresses.ps1 -UseBasicParsing -OutFile .\docker_support\Get-IPAddresses.ps1
