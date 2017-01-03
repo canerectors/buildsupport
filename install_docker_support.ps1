d0b6d80b5fd507d12a199c1666e0051f905fe6fa
James Fera <james.fera@gmail.com>
2017-01-03 17:07:36 -0500

    docker support files

--- install_docker_support.ps1
+++ install_docker_support.ps1
@@ -1,11 +1,11 @@
 Write-Host "Installing Docker Support files to: $(Get-Location)\docker_support..."
 
+md .\docker_support *> $null
+
 #install dependencies
 iwr https://raw.githubusercontent.com/canerectors/buildsupport/master/docker_support/Disable-WindowsContainers.ps1 -UseBasicParsing -OutFile .\docker_support\Disable-WindowsContainers.ps1
 iwr https://raw.githubusercontent.com/canerectors/buildsupport/master/docker_support/Enable-WindowsContainers.ps1 -UseBasicParsing -OutFile .\docker_support\Enable-WindowsContainers.ps1
 iwr https://raw.githubusercontent.com/canerectors/buildsupport/master/docker_support/install_dependencies.ps1 -UseBasicParsing | iex
-
-md .\docker_support *> $null
 
 iwr https://raw.githubusercontent.com/canerectors/buildsupport/master/docker_support/docker_commands.ps1 -UseBasicParsing -OutFile .\docker_support\docker_commands.ps1
 iwr https://raw.githubusercontent.com/canerectors/buildsupport/master/docker_support/setup.ps1 -UseBasicParsing -OutFile .\docker_support\setup.ps1
