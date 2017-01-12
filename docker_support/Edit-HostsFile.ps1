aae5c7f2cda7b0b40bc3d5a851b596197395b9c9
James Fera <james.fera@gmail.com>
2017-01-12 13:36:16 -0500

    add services to hosts file

--- docker_support/Edit-HostsFile.ps1
+++ docker_support/Edit-HostsFile.ps1
@@ -2,6 +2,11 @@ $services = .\docker_support\Get-Services.ps1
 
 $hostsPath = "$env:windir\System32\drivers\etc\hosts"
 
+$hostsPathOrig = $hostsPath + ".orig"
+
+#backup original hosts file
+if(!(Test-Path ($hostsPathOrig))){ copy $hostsPath $hostsPathOrig }
+
 $hosts = get-content $hostsPath
 
 #remove existing entries
@@ -21,6 +26,6 @@ $hosts = $hosts | Foreach {
     }
 
 #append docker services to hosts file
-$services | Foreach { $hosts += $_.IPAddress + "`t" + $_.ServiceName  + "`t`t#" + $_.Name}
+$services | Foreach { $hosts += $_.IPAddress + "`t" + $_.Name  + "`t`t#" + $_.Name}
 
 $hosts | Out-File $hostsPath -enc ascii
