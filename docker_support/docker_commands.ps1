96eab6466a87b53f7e4f85de7abdcfce71d45afb
James Fera <james.fera@gmail.com>
2017-03-27 09:42:19 -0400

    menu

--- docker_support/docker_commands.ps1
+++ docker_support/docker_commands.ps1
@@ -12,12 +12,13 @@ while(-not $done)
 
         Write-Host "Select An Action:`n"
         Write-Host "1) Run Development Environment Docker Containers"
-		Write-Host "2) Pull New Images"
-        Write-Host "3) Pause Docker Containers"
-        Write-Host "4) Remove Docker Containers (This will destroy all data located inside the containers)" -ForegroundColor Red
-        Write-Host "5) View Container Urls"
-	    Write-Host "6) View Container Status"
-        Write-Host "7) Launch Containers"
+		Write-Host "2) Pull New Images"        
+		Write-Host "3) Launch Containers"        
+        Write-Host "4) View Container Urls"
+	    Write-Host "5) View Container Status"
+		Write-Host "6) Pause Docker Containers"
+		Write-Host
+        Write-Host "7) Remove Docker Containers (This will destroy all data located inside the containers)" -ForegroundColor Red
 	    #Write-Host "7) Docker Zap" -ForegroundColor Red
         #Write-Host "7) Restart Explorer"
 
@@ -34,10 +35,10 @@ while(-not $done)
         1 {& .\docker_support\setup.ps1 } 
 		2 {& docker-compose -f .\docker-compose.yaml pull }
         3 {& docker-compose stop } 
-        4 {& docker-compose down } 
+        4 {& .\docker_support\launch.ps1 }
         5 {& .\docker_support\Get-Services.ps1 | Format-Table Name, Url, IPAddress }
-		6 {& docker-compose ps }
-        7 {& .\docker_support\launch.ps1 }
+		6 {& docker-compose ps }        
+		7 {& docker-compose down } 
 		#7 {& net stop docker; .\docker_support\docker_zap\docker-ci-zap -folder C:\ProgramData\Docker; net start docker }
 		#7 {& .\docker_support\launch-consolelogger.ps1 } 
         #7 {& cmd.exe /c "taskkill /IM explorer.exe /F"; & cmd.exe /c "explorer.exe"}  
