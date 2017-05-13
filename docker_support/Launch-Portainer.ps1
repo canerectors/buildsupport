2b9641e0ec68764acc57db21e36dd123308a91ba
James Fera <james.fera@gmail.com>
2017-05-13 15:06:16 -0400

    start portainer if stopped

--- docker_support/Launch-Portainer.ps1
+++ docker_support/Launch-Portainer.ps1
@@ -5,6 +5,7 @@ $ip=(Get-NetIPAddress -AddressFamily IPv4 | Where-Object -FilterScript { $_.Inte
 $dockerHost = "tcp://" + $ip + ":2375"
 
 docker run -d --name portainer portainer/portainer -H $dockerHost --no-auth >$null 2>&1
+docker start portainer >$null 2>&1
 
 $service = Invoke-Expression "$PSScriptRoot\Get-Services.ps1 portainer"
 
