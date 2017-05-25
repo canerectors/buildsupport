f7af5c73f892556dbfb4eaae0813680739ad6817
James Fera <james.fera@gmail.com>
2017-05-25 18:31:08 -0400

    pin portainer version

--- docker_support/Launch-Portainer.ps1
+++ docker_support/Launch-Portainer.ps1
@@ -1,10 +1,10 @@
-docker pull portainer/portainer
+docker pull portainer/portainer:windows-amd64-1.13.0
 
 $ip=(Get-NetIPAddress -AddressFamily IPv4 | Where-Object -FilterScript { $_.InterfaceAlias -Eq "vEthernet (HNS Internal NIC)" }).IPAddress
 
 $dockerHost = "tcp://" + $ip + ":2375"
 
-docker run -d --name portainer portainer/portainer -H $dockerHost --no-auth >$null 2>&1
+docker run -d --name portainer portainer/portainer:windows-amd64-1.13.0 -H $dockerHost --no-auth >$null 2>&1
 docker start portainer >$null 2>&1
 
 $service = Invoke-Expression "$PSScriptRoot\Get-Services.ps1 portainer"
